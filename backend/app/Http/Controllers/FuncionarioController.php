dc568c0aabbdadbb4fe38e324fb25ee80de00e04
Nivs <nivsoficial@gmail.com>
2023-07-30 17:59:27 -0300

    feat: [BACKEND] init on models/controllers/services/factories/seeders/polcies/requests/migrations (for Funcionario, Departamento, and Tarefa)

---
+++ backend/app/Http/Controllers/FuncionarioController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFuncionarioRequest;
+use App\Http\Requests\UpdateFuncionarioRequest;
+use App\Models\Funcionario;
+
+class FuncionarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFuncionarioRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Funcionario $funcionario)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Funcionario $funcionario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFuncionarioRequest $request, Funcionario $funcionario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Funcionario $funcionario)
+    {
+        //
+    }
+}
