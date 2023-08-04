99659858f836902d700dfb5033dc0415f5bd2b85
Nivs <nivsoficial@gmail.com>
2023-08-04 00:04:19 -0300

    fix: [FRONTEND] Dependencies / Dashboard :: added redux toolkit & bootstrap, atts on dashboard [WIP]

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,17 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {RouterProvider} from "react-router-dom";
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.css';
 import router from './router';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
+
+
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,2 +1,17 @@
-const Dashboard = () => (<> Dashboard page </>);
+import { useEffect } from "react";
+import DashboardComponents from "../components/Dashboard/index";
+import { useDispatch, useSelector } from "react-redux";
+import { dashboardActions } from "../store/dashboard-slice";
+import tarefaData from "../data/Tarefa";
+
+const Dashboard = () => {
+    return (<> 
+        <div className={`d-flex p-3`}>
+            <DashboardComponents.Buttons/>
+            <DashboardComponents.Search/>
+        </div>
+
+        <DashboardComponents.List/>
+    </>)
+};
 export default Dashboard;
